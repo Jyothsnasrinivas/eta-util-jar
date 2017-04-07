0481c862abbf84b2b4efe1a0c4d7593a03c85fb3
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-07 09:13:05 +0530

    Imported Attributes and Attributes.Name

--- src/Jar/Types.hs
+++ src/Jar/Types.hs
@@ -6,9 +6,9 @@ import Java.Collections
 import Java.Exception
 import Java.IO
 
--- Start java.util.zip.Pack200.Packer
+-- Start java.util.jar.Pack200.Packer
 
-data {-# CLASS "java.util.zip.Pack200$Packer" #-}
+data {-# CLASS "java.util.jar.Pack200$Packer" #-}
   Packer = Packer (Object# Packer)
   deriving Class
 
@@ -22,9 +22,9 @@ foreign import java unsafe "@interface"
 
 -- End java.util.zip.Pack200.Packer
 
--- Start java.util.zip.Pack200.Unpacker
+-- Start java.util.jar.Pack200.Unpacker
 
-data {-# CLASS "java.util.zip.Pack200$Unpacker" #-}
+data {-# CLASS "java.util.jar.Pack200$Unpacker" #-}
   Unpacker = Unpacker (Object# Unpacker)
   deriving Class
 
@@ -36,3 +36,25 @@ foreign import java unsafe "@interface unpack"
 
 foreign import java unsafe "@interface unpack"
   unpack2 :: (b <: Unpacker) => InputStream -> JarOutputStream
+
+-- End java.util.jar.Pack200.Unpacker
+
+-- Start java.util.jar.Attributes
+
+data {-# CLASS "java.util.jar.Attributes" #-}
+  Attributes = Attributes (Object# Attributes)
+  deriving Class
+
+type instance Inherits Attributes = '[Object, (Map Object Object)]
+
+-- End java.util.jar.Attributes
+
+-- Start java.util.jar.Attributes.Name
+
+data {-# CLASS "java.util.jar.Attributes$Name" #-}
+  Name = Name (Object# Name)
+  deriving Class
+
+type instance Inherits Name = '[Object, Attributes]
+
+-- End java.util.jar.Attributes.Name
