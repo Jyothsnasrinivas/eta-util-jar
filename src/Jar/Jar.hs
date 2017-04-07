0d0fa71c1cc13e626124c35b47ca0c19b9981659
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-04-07 09:31:34 +0530

    Imported JarInputStream

--- src/Jar/Jar.hs
+++ src/Jar/Jar.hs
@@ -5,6 +5,19 @@ import Java.Array
 import Java.IO
 import Zip.Zip
 
+
+data {-# CLASS "java.security.cert.Certificate[]" #-}
+  CertificateArray = CertificateArray (Object# CertificateArray)
+  deriving Class
+
+instance JArray Certificate CertificateArray
+
+data {-# CLASS "java.security.CodeSigner[]" #-}
+  CodeSignerArray = CodeSignerArray (Object# CodeSignerArray)
+  deriving Class
+
+instance JArray Certificate CertificateArray
+
 -- Start java.util.jar.JarFile
 
 data {-# CLASS "java.util.jar.JarFile" #-}
@@ -14,3 +27,29 @@ data {-# CLASS "java.util.jar.JarFile" #-}
 type instance Inherits JarFile = '[ZipFile, Closeable]
 
 -- End java.util.jar.JarFile
+
+-- Start java.util.jar.JarEntry
+
+data {-# CLASS "java.util.jar.JarEntry" #-}
+  JarEntry = JarEntry (Object# JarEntry)
+  deriving Class
+
+type instance Inherits JarEntry = '[ZipEntry]
+
+foreign import java unsafe getAttributes :: Java JarEntry Attributes
+
+foreign import java unsafe getCertificates :: Java JarEntry CertificateArray
+
+foreign import java unsafe getCodeSigners :: Java JarEntry CodeSignerArray
+
+-- End java.util.jar.JarFile
+
+-- Start java.util.jar.JarInputStream
+
+data {-# CLASS "java.util.jar.JarInputStream" #-}
+  JarInputStream = JarInputStream (Object# JarInputStream)
+  deriving Class
+
+type instance Inherits JarInputStream = '[ZipInputStream, Closeable]
+
+-- End java.util.jar.JarInputStream
